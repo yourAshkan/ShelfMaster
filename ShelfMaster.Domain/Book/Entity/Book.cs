90a6507e92e6675c24303a99c522ce35fad1b25e
yourAshkan <ashkan.kiya11@gmail.com>
2025-10-02 12:11:01 +0330

    TST - 1 - Init Onion Architecture Layers to Solution

---
+++ ShelfMaster.Domain/Book/Entity/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace ShelfMaster.Domain.Book.Entity;
+
+public class Book
+{
+    public int Id { get; private set; }
+    public string Title { get; private set; }
+    public string Author { get; private set; }
+    public string Image { get; private set; }
+    public string Description { get; private set; }
+    public bool IsAvailable { get; set; } = true;
+
+    public Book()
+    {
+        
+    }
+}
---
+++ ShelfMaster.Domain/User/Entity/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ShelfMaster.Domain.User.Entity;
+
+public class User
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string Family { get; private set; }
+    public string NationalCode { get; private set; }
+    public string PhoneNumber { get; private set; }
+    public string? Email { get; private set; }
+
+    public User()
+    {
+
+    }
+}
